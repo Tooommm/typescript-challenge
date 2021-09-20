dcd7e3f86cf1fb0a27fc8b01aeef7265821aae38
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-09-20 11:31:47 +0200

    correction exercice

--- build/hello.js
+++ build/hello.js
@@ -6,17 +6,12 @@ const hello = (name) => {
 console.log(hello(myName));
 const prettyPrintWilder = (users) => {
     users.map((user) => {
-        if (user.age) {
-            console.log(`${user.name} is ${user.age} years old`);
-        }
-        else {
-            console.log(`${user.name} birthday ${user.birthday}`);
-        }
+        console.log(`${user.name} is ${user.age} years old`);
     });
 };
 const wilders = [];
 const user1 = { name: "Pierre", age: 23 };
-const user2 = { name: "Paul", birthday: "10/02/1990" };
+const user2 = { name: "Paul", age: 31, birthday: "10/02/1990" };
 const user3 = { name: "Jacques", age: 25 };
 wilders.push(user1);
 wilders.push(user2);
--- hello.ts
+++ hello.ts
@@ -9,23 +9,19 @@ console.log(hello(myName));
 // challenge.ts
 interface User {
   name: string;
-  age?: number;
+  age: number;
   birthday?: string;
 }
 
 const prettyPrintWilder = (users: User[]) => {
-  users.map((user) => {
-    if (user.age) {
-      console.log(`${user.name} is ${user.age} years old`);
-    } else {
-      console.log(`${user.name} birthday ${user.birthday}`);
-    }
+  users.map((user: User) => {
+    console.log(`${user.name} is ${user.age} years old`);
   });
 };
 
 const wilders: User[] = [];
 const user1: User = { name: "Pierre", age: 23 };
-const user2: User = { name: "Paul", birthday: "10/02/1990" };
+const user2: User = { name: "Paul", age: 31, birthday: "10/02/1990" };
 const user3: User = { name: "Jacques", age: 25 };
 wilders.push(user1);
 wilders.push(user2);
